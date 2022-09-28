b4ad0c574b0d891fc45eb9886766afb53896fa0b
sairaj-fc <sairaj@findcoder.io>
2022-09-28 12:47:30 +0530

    fix the resize window remaps, add telescope diagnostics command

--- lua/sairaj/keymaps.lua
+++ lua/sairaj/keymaps.lua
@@ -17,13 +17,14 @@ keymap('n', "<C-k>", "<C-w>k", opts)
 keymap('n', "<C-l>", "<C-w>l", opts)
 
 -- resize windows
-keymap("n", "<C-Up>", ":resize -2<CR>", opts)
-keymap("n", "<C-Down>", ":resize +2<CR>", opts)
-keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
-keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
+keymap("n", "<C-S-Up>", ":resize -2<CR>", opts)
+keymap("n", "<C-S-Down>", ":resize +2<CR>", opts)
+keymap("n", "<C-S-Left>", ":vertical resize -2<CR>", opts)
+keymap("n", "<C-S-Right>", ":vertical resize +2<CR>", opts)
 
 -- telescope
 keymap('n', "<leader>ff", "<cmd>lua require('telescope.builtin').find_files()<cr>", opts)
 keymap('n', "<leader>fg", "<cmd>lua require('telescope.builtin').live_grep()<cr>", opts)
 keymap('n', "<leader>fb", "<cmd>lua require('telescope.builtin').buffers()<cr>", opts)
 keymap('n', "<leader>fh", "<cmd>lua require('telescope.builtin').help_tags()<cr>", opts)
+keymap('n', "<leader>dl", "<cmd>lua require('telescope.builtin').diagnostics()<cr>", opts)
