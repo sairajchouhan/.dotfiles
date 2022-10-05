fd2806d9e4459046db945ae71d011ea534e0c929
sairaj-fc <sairaj@findcoder.io>
2022-10-06 01:39:40 +0530

    add tokyo night theme

--- lua/sairaj/colorschemes/tokyo.lua
+++ lua/sairaj/colorschemes/tokyo.lua
@@ -1,5 +1,6 @@
 require("tokyonight").setup({
-  style = "moon" --  `storm`, `moon`, a darker variant `night` and `day`
+  style = "moon", -- `storm`, `moon`, a darker variant `night` and `day`
+  transparent = false
 })
 
 vim.cmd[[colorscheme tokyonight]]
