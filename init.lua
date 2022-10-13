1187c44ddcb387f71cdde51e4aa4039e9fbcea7f
sairaj-fc <sairaj@findcoder.io>
2022-10-13 16:36:50 +0530

    sync then yanked text to system clipboard

--- init.lua
+++ init.lua
@@ -58,3 +58,21 @@ a.nvim_create_autocmd('TextYankPost', {
     end,
 })
 
+
+local has = function(x)
+  return vim.fn.has(x) == 1
+end
+
+
+local is_mac = has "macunix"
+local is_windows = has "win32" 
+
+
+if is_mac then
+  vim.opt.clipboard:append { 'unnamedplus' }
+end
+
+if is_windows then
+  vim.opt.clipboard:prepend { 'unnamed', 'unnamedplus' }
+end
+
