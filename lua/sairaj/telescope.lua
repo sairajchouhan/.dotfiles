b0a8bb0229ad8e3510b06ea4ff588dccc80a3e06
sairajchouhan <sairaj2119@gmail.com>
2023-01-07 00:01:44 +0530

    fix the telescope propmt the way I want

--- lua/sairaj/telescope.lua
+++ lua/sairaj/telescope.lua
@@ -10,45 +10,14 @@ telescope.setup {
   defaults = {
     prompt_prefix = icons.ui.Telescope .. " ",
     path_display = { "smart" },
+    sorting_strategy = "ascending",
+    layout_config = {
+      prompt_position = "top"
+    },
     mappings = {
       n = {
         ["q"] = actions.close,
-        ["esc"] = actions.close,
       },
     }
-  },
-  pickers = {
-    find_files = {
-      theme = "dropdown",
-      previewer = false,
-      layout_config = {
-        prompt_position = "top",
-      },
-    },
-    diagnostics = {
-      theme = "dropdown",
-      previewer = false,
-      layout_config = {
-        prompt_position = "top",
-        width = 0.75,
-        height = 0.75,
-      },
-    },
-
-   -- Default configuration for builtin pickers goes here:
-    -- picker_name = {
-    --   picker_config_key = value,
-    --   ...
-    -- }
-    -- Now the picker_config_key will be applied every time you call this
-    -- builtin picker
-  },
-  extensions = {
-
-    -- Your extension configuration goes here:
-    -- extension_name = {
-    --   extension_config_key = value,
-    -- }
-    -- please take a look at the readme of the extension you want to configure
-  },
+  }
 }
