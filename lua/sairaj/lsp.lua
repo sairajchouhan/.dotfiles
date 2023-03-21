148e9de01ee9713b3fd9ee05c05f0559d5485863
sairaj <sairaj2119@gmail.com>
2023-03-21 12:15:12 +0530

    remove unused

--- lua/sairaj/lsp.lua
+++ lua/sairaj/lsp.lua
@@ -73,19 +73,6 @@ local on_attach = function(_, bufnr)
     vim.cmd("normal! zz")
   end, "Go to previous error diagnostic")
 
-  ------------------- Lesser used LSP functionality personally --------------------------------------
-  nmap('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
-  nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
-  nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
-  nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
-  nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-  nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
-  nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
-  nmap('<leader>wl', function()
-    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-  end, '[W]orkspace [L]ist Folders')
-  --------------------------------------------------------------------------------------------------
-
   -- Create a command `:Format` local to the LSP buffer
   vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
     vim.lsp.buf.format()
