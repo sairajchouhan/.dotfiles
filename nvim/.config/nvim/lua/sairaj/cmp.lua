0c88335ffc772240dfd29e103e74c1740e29e0da
sairaj <sairaj2119@gmail.com>
2023-05-28 18:20:44 +0530

    some stuff

--- nvim/.config/nvim/lua/sairaj/cmp.lua
+++ nvim/.config/nvim/lua/sairaj/cmp.lua
@@ -43,12 +43,7 @@ cmp.setup {
   },
   sources = {
     { name = 'nvim_lsp' },
+    { name = 'path' },
     { name = 'luasnip' },
   },
-  formatting = {
-    format = function(_, vim_item)
-      vim_item.abbr = string.sub(vim_item.abbr, 1, 20)
-      return vim_item
-    end
-  }
 }
