a119ebe38d5d57dc6768e224dd0181cf060b0569
sairaj <sairaj2119@gmail.com>
2024-05-31 10:20:06 +0530

    got border to cmp and some remaps in lsp

--- nvim/.config/nvim/lua/sairaj/plugins/lsp.lua
+++ nvim/.config/nvim/lua/sairaj/plugins/lsp.lua
@@ -49,14 +49,22 @@ return { -- LSP Configuration & Plugins
         -- Jump to the definition of the word under your cursor.
         --  This is where a variable was first declared, or where a function is defined, etc.
         --  To jump back, press <C-t>.
-        map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
+        map('gd', function()
+          require('telescope.builtin').lsp_definitions {}
+          vim.cmd 'normal! zz'
+        end, '[G]oto [D]efinition')
 
         -- Find references for the word under your cursor.
-        map('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+        map('gr', function()
+          require('telescope.builtin').lsp_references {}
+        end, '[G]oto [R]eferences')
 
         -- Jump to the implementation of the word under your cursor.
         --  Useful when your language has ways of declaring types without an actual implementation.
-        map('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
+        map('gI', function()
+          require('telescope.builtin').lsp_implementations {}
+          vim.cmd 'normal! zz'
+        end, '[G]oto [I]mplementation')
 
         -- Jump to the type of the word under your cursor.
         --  Useful when you're not sure what type a variable is and you want to see
@@ -86,6 +94,36 @@ return { -- LSP Configuration & Plugins
         -- WARN: This is not Goto Definition, this is Goto Declaration.
         --  For example, in C this would take you to the header.
         map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+
+        map('[d', function()
+          vim.diagnostic.goto_prev()
+          vim.cmd 'normal! zz'
+        end, 'Go to next diagnostic')
+
+        map(']d', function()
+          vim.diagnostic.goto_next()
+          vim.cmd 'normal! zz'
+        end, 'Go to previous diagnostic')
+
+        map('[e', function()
+          vim.diagnostic.goto_prev {
+            severity = vim.diagnostic.severity.ERROR,
+          }
+          vim.cmd 'normal! zz'
+        end, 'Go to next error diagnostic')
+
+        map(']e', function()
+          vim.diagnostic.goto_next {
+            severity = vim.diagnostic.severity.ERROR,
+          }
+          vim.cmd 'normal! zz'
+        end, 'Go to previous error diagnostic')
+
+        map(';d', function()
+          vim.diagnostic.open_float {
+            scope = 'line',
+          }
+        end, 'Show line diagnostics')
 
         -- The following two autocommands are used to highlight references of the
         -- word under your cursor when your cursor rests there for a little while.
@@ -149,6 +187,10 @@ return { -- LSP Configuration & Plugins
             completion = {
               callSnippet = 'Replace',
             },
+            diagnostics = {
+              globals = { 'P' },
+            },
+
             -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
             -- diagnostics = { disable = { 'missing-fields' } },
           },
--- nvim/.config/nvim/lua/sairaj/remaps.lua
+++ nvim/.config/nvim/lua/sairaj/remaps.lua
@@ -31,8 +31,6 @@ keymap('v', 'p', '"_dP', opts) -- do not update the register on paste
 keymap('n', '<leader>bl', ':BlameLineOnce<cr>', opts)
 
 -- Diagnostic keymaps
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
 -- vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
 -- vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
