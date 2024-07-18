6c79b6ff8ec82ae5791444ebd8aa79dcceb5b865
sairaj <sairaj2119@gmail.com>
2024-07-18 19:03:09 +0530

    add next todo

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -91,7 +91,29 @@ require('lazy').setup({
   },
 
   -- Highlight todo, notes, etc in comments
-  { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = true } },
+  {
+    'folke/todo-comments.nvim',
+    event = 'VimEnter',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    opts = { signs = true },
+    keys = {
+      {
+        ']t',
+        function()
+          require('todo-comments').jump_next()
+        end,
+        desc = 'Next Todo Comment',
+      },
+      {
+        '[t',
+        function()
+          require('todo-comments').jump_prev()
+        end,
+        desc = 'Previous Todo Comment',
+      },
+      { '<leader>ft', '<cmd>TodoTelescope<cr>', desc = 'Todo search telescope' },
+    },
+  },
 
   { 'nvim-treesitter/nvim-treesitter-context' },
 
