dcd19dc850ee60f9a9858b450465b7f493449b70
sairaj <sairaj2119@gmail.com>
2024-07-20 12:24:06 +0530

    setup  mason to have borders

--- nvim/.config/nvim/lua/sairaj/plugins/lsp.lua
+++ nvim/.config/nvim/lua/sairaj/plugins/lsp.lua
@@ -204,7 +204,11 @@ return { -- LSP Configuration & Plugins
     --    :Mason
     --
     --  You can press `g?` for help in this menu.
-    require('mason').setup()
+    require('mason').setup {
+      ui = {
+        border = 'rounded',
+      },
+    }
 
     -- You can add other tools here that you want Mason to install
     -- for you, so that they are available from within Neovim.
