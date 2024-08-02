d0fbb5cb9e55cc55ad2f9ec275ba9e593f1cdc90
sairaj <sairaj2119@gmail.com>
2024-08-02 20:29:48 +0530

    add bufferline cycling

--- nvim/.config/nvim/lua/plugins/bufferline.lua
+++ nvim/.config/nvim/lua/plugins/bufferline.lua
@@ -1,8 +1,12 @@
--- TODO: add [b and ]b mappings to go through buffers
 return {
   "akinsho/bufferline.nvim",
   version = "*",
   dependencies = 'nvim-tree/nvim-web-devicons',
-  opts = {}
+  config = function ()
+    require('bufferline').setup({})
+    vim.api.nvim_set_keymap('n', '<leader>co', '<cmd>BufferLineCloseOthers<cr>', { noremap = true, silent = true })
+    vim.api.nvim_set_keymap('n', '[b', '<cmd>BufferLineCyclePrev<cr>', { noremap = true, silent = true })
+    vim.api.nvim_set_keymap('n', ']b', '<cmd>BufferLineCycleNext<cr>', { noremap = true, silent = true })
+  end
 }
 
