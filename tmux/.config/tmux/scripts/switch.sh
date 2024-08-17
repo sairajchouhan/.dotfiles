ac76380996e71f473b4d1d409991070ba07f09d8
sairaj <sairaj2119@gmail.com>
2024-08-17 21:35:51 +0530

    clean up

--- tmux/.config/tmux/scripts/switch.sh
+++ tmux/.config/tmux/scripts/switch.sh
@@ -17,7 +17,6 @@ trap cleanup_and_exit SIGINT
 #   sed 's/.*\///' | 
 #   sed '/^$/d' | 
 #   fzf-tmux -p
-# --query "$current_session" --select-1
 
 current_session=$(tmux display-message -p '#S')
 line_number=$(tmux ls | grep -n "$current_session" | cut -d ':' -f 1)
@@ -27,16 +26,15 @@ if [[ -n "$selected_session"  ]]; then
   tmux switch-client -t "$selected_session"
 fi
 
-# tmux ls | cut -d ":" -f 1 | fzf-tmux -p | xargs tmux switch-client -t
 
 
-
-# Check the exit status
-exit_status=$?
-
-# If fzf-tmux was interrupted (status 130), exit cleanly
-if [ $exit_status -eq 130 ]; then
-    exit 0
-else
-    exit $exit_status
-fi
+######## I might not need this ########
+# # Check the exit status
+# exit_status=$?
+#
+# # If fzf-tmux was interrupted (status 130), exit cleanly
+# if [ $exit_status -eq 130 ]; then
+#     exit 0
+# else
+#     exit $exit_status
+# fi
