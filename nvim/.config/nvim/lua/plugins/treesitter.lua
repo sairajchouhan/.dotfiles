3c94f8c9138457410989d006ce53c022c8ce4ccb
sairaj <sairaj2119@gmail.com>
2024-08-24 09:50:29 +0530

    remove inc sel

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -35,16 +35,6 @@ return {
       additional_vim_regex_highlighting = false,
     },
     indent = { enable = true },
-    incremental_selection = {
-      enable = true,
-      keymaps = {
-        init_selection = '<C-space>',
-        node_incremental = '<C-space>',
-        -- TODO: have to test this option
-        scope_incremental = false,
-        node_decremental = '<bs>',
-      },
-    },
   },
   config = function(_, opts)
     require('nvim-treesitter.configs').setup(opts)
