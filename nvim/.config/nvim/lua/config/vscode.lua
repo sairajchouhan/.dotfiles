51affce4d43747568ad10a0ab93cd4db2309d153
sairaj <sairaj2119@gmail.com>
2024-09-07 10:27:16 +0530

    adds hilight on yank for neovim in vscode

---
+++ nvim/.config/nvim/lua/config/vscode.lua
@@ -0,0 +1,11 @@
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking text',
+  group = vim.api.nvim_create_augroup('highlight_yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank {
+      timeout = 30,
+    }
+  end,
+})
+
+return {}
