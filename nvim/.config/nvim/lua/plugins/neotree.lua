dc1a76c6334efe8ce214b27ea776ad5f3a0c6c92
sairaj <sairaj2119@gmail.com>
2024-10-10 09:29:30 +0530

    chore: set nvim tree copy things

--- nvim/.config/nvim/lua/plugins/neotree.lua
+++ nvim/.config/nvim/lua/plugins/neotree.lua
@@ -7,8 +7,8 @@ local function copy_path(state)
   local modify = vim.fn.fnamemodify
 
   local results = {
-    filepath,
     modify(filepath, ':.'),
+    filepath,
     modify(filepath, ':~'),
     filename,
     modify(filename, ':r'),
@@ -16,8 +16,8 @@ local function copy_path(state)
   }
 
   vim.ui.select({
-    '1. Path relative to CWD: ' .. results[2],
-    '2. Absolute path: ' .. results[1],
+    '1. Path relative to CWD: ' .. results[1],
+    '2. Absolute path: ' .. results[2],
     '3. Path relative to HOME: ' .. results[3],
     '4. Filename: ' .. results[4],
     '5. Filename without extension: ' .. results[5],
