cc3ea199a867993046eb3824c4fa081532bb8e96
sairaj <sairaj2119@gmail.com>
2024-12-25 20:33:33 +0530

    basic neovim setup for vscode

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,4 +1,5 @@
 if vim.g.vscode then
+  require 'config.vscode'
   return
 end
 
--- nvim/.config/nvim/lua/config/vscode.lua
+++ nvim/.config/nvim/lua/config/vscode.lua
@@ -1,11 +1,18 @@
+vim.api.nvim_set_hl(0, 'YankHighlight', { bg = '#e0af68', fg = 'NONE' })
+
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking text',
   group = vim.api.nvim_create_augroup('highlight_yank', { clear = true }),
   callback = function()
     vim.highlight.on_yank {
       timeout = 30,
+      higroup = 'YankHighlight',
     }
   end,
 })
+
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+vim.opt.clipboard = 'unnamedplus'
 
 return {}
