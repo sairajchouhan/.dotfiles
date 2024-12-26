561bebe8a58082e79f3f28d72645544b03e8433d
sairaj <sairaj2119@gmail.com>
2024-12-26 22:16:04 +0530

    beautiful neovim bindings in vscode, best of both worlds

--- nvim/.config/nvim/lua/config/vscode.lua
+++ nvim/.config/nvim/lua/config/vscode.lua
@@ -1,3 +1,8 @@
+local M = {}
+
+local keymap = vim.keymap.set
+local vscode = require 'vscode'
+
 vim.api.nvim_set_hl(0, 'YankHighlight', { bg = '#e0af68', fg = 'NONE' })
 
 vim.api.nvim_create_autocmd('TextYankPost', {
@@ -15,4 +20,52 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 vim.opt.clipboard = 'unnamedplus'
 
-return {}
+local opts = { noremap = true, silent = true }
+
+keymap('n', '<cr>', '<cmd>noh<cr><cr>', opts)
+keymap('n', 'x', '"_x', opts) -- do not yank on x
+keymap('v', 'p', '"_dP', opts) -- do not update the register on paste
+
+vim.keymap.set('n', 'gr', function()
+  vscode.action 'editor.action.referenceSearch.trigger'
+end)
+
+vim.keymap.set('n', '<leader>rn', function()
+  vscode.action 'editor.action.rename'
+end)
+
+vim.keymap.set('n', '[c', function()
+  vscode.action 'workbench.action.editor.previousChange'
+end)
+
+vim.keymap.set('n', ']c', function()
+  vscode.action 'workbench.action.editor.nextChange'
+end)
+
+vim.keymap.set('n', '[d', function()
+  vscode.action 'editor.action.marker.prev'
+end)
+
+vim.keymap.set('n', ']d', function()
+  vscode.action 'editor.action.marker.next'
+end)
+
+-- show current line error
+vim.keymap.set('n', ';d', function()
+  vim.cmd.normal { '0', bang = true }
+  vscode.action 'editor.action.marker.next'
+end)
+
+vim.keymap.set('n', '[e', function()
+  vscode.action 'go-to-next-error.prev.error'
+end)
+
+vim.keymap.set('n', ']e', function()
+  vscode.action 'go-to-next-error.next.error'
+end)
+
+vim.keymap.set('n', 'F', function()
+  vscode.action 'editor.action.formatDocument'
+end)
+
+return M
