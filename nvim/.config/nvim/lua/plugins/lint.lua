38c231d63360c020dc94edb94f292283cacfe6a8
sairaj <sairaj2119@gmail.com>
2025-05-25 09:27:28 +0530

    added hlslens plugin

--- nvim/.config/nvim/lua/plugins/lint.lua
+++ nvim/.config/nvim/lua/plugins/lint.lua
@@ -11,7 +11,7 @@ return {
       -- javascriptreact = { 'eslint_d' },
       -- typescriptreact = { 'eslint_d' },
       -- svelte = { 'eslint_d' },
-      go = { 'staticcheck' },
+      -- go = { 'golangcilint' },
     }
 
     local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
@@ -19,6 +19,7 @@ return {
     vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
       group = lint_augroup,
       callback = function()
+        -- print 'trying to lint'
         lint.try_lint()
       end,
     })
