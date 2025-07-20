97f6108f569961a1e6fcbd1a2f28d0c27f572299
sairaj <sairaj2119@gmail.com>
2025-07-20 21:09:32 +0530

    updates

--- nvim/.config/nvim/lua/plugins/noice.lua
+++ nvim/.config/nvim/lua/plugins/noice.lua
@@ -8,5 +8,10 @@ return {
     presets = {
       lsp_doc_border = true, -- add a border to hover docs and signature help
     },
+    views = {
+      popup = { scrollbar = false },
+      split = { scrollbar = false },
+      mini = { scrollbar = false },
+    },
   },
 }
