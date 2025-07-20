a24ea24b288d3d14466c2656aa4ef74b200526a0
sairaj <sairaj2119@gmail.com>
2025-07-20 21:09:32 +0530

    updates

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -28,6 +28,9 @@ return {
             [vim.diagnostic.severity.INFO] = "I",
           },
         },
+        float = {
+          border = "rounded",
+        },
       },
       -- Enable this to enable the builtin LSP inlay hints on Neovim >= 0.10.0
       -- Be aware that you also will need to properly configure your LSP server to
