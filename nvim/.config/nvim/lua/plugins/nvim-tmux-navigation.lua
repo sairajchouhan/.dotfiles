015a6855e7daf55662c754362d2189f6dd004f37
sairaj <sairaj2119@gmail.com>
2025-07-30 12:57:51 +0530

    updates

---
+++ nvim/.config/nvim/lua/plugins/nvim-tmux-navigation.lua
@@ -0,0 +1,17 @@
+-- TODO: not working fix this
+return {
+  "alexghergh/nvim-tmux-navigation",
+  config = function()
+    require("nvim-tmux-navigation").setup({
+      disable_when_zoomed = true,
+      keybindings = {
+        left = "<C-h>",
+        down = "<C-j>",
+        up = "<C-k>",
+        right = "<C-l>",
+        last_active = "<C-\\>",
+        -- next = '<C-Space>',
+      },
+    })
+  end,
+}
