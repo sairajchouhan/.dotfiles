c7a2009f249711ed52d8c926201d7a8c8908dd24
sairaj <sairaj2119@gmail.com>
2025-07-31 10:21:47 +0530

    keymaps updates

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -20,5 +20,7 @@ vim.keymap.set("n", "<C-S-Up>", "<cmd>resize +2<cr>", { desc = "Increase Window 
 vim.keymap.set("n", "<C-S-Down>", "<cmd>resize -2<cr>", { desc = "Decrease Window Height" })
 vim.keymap.set("n", "<C-S-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease Window Width" })
 vim.keymap.set("n", "<C-S-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase Window Width" })
+vim.keymap.set("n", "x", '"_x', opts) -- do not yank on x
+vim.keymap.set("v", "p", '"_dP', opts) -- do not update the register on paste
 
 -- https://www.lazyvim.org/keymaps#bufferlinenvim
